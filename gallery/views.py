8779b6a07a5956be693d28d03c94f2693065fac9
Oromishh <olexandrhorobets@knu.ua>
2023-05-23 14:07:23 +0300

    Add galery view.

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+
+
+# Необхідно додати до аплікації gallery дві views.
+#  - призначення: відображає зображення, які були створені в останній місяць.
+#  - назва view: gallery_view
+#  - шаблон: gallery.html
+
+#  Add views.gallery_view
+#  Add views.image_detail
+
+def gallery_view(request):
+    return render(request, 'gallery.html')
+
+def image_detail(request, pk):
+    return render(request, 'image_detail.html')
+
