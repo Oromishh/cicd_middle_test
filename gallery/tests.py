57de65f5e06adc051f50ca67b930f5c4a4defc18
Oromishh <olexandrhorobets@knu.ua>
2023-05-23 14:34:07 +0300

    Added tests.

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+#  Create tests for views.gallery_view and views.image_detail
+
+
+class GalleryViewTest(TestCase):
+    def test_gallery_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'gallery.html')
+        self.assertContains(response, 'Cat Photo 1')
+        self.assertNotContains(response, 'Dog Photo 2')
+        self.assertNotContains(response, 'Cat Photo 3')
+        self.assertNotContains(response, 'Dog Photo 4')
+
+
+class ImageDetailViewTest(TestCase):
+    def test_image_detail_view(self):
+        response = self.client.get('/image/1/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'image_detail.html')
+        self.assertContains(response, 'ID: 1')
+        self.assertContains(response, '1.jpg')
+        self.assertNotContains(response, 'ID: 2')
+        self.assertNotContains(response, '2.jpg')
